53bb51789b35361e57f6c3dc3f494e8bdcdb0657
SimmonsCS28 <SimmonsCS28@gmail.com>
2016-03-29 10:53:54 -0500

    Tweet Search Function
    Added code to search an entire tweet for hashtags that can then be
    checked against the master list.

--- MainTweetSearch.java
+++ MainTweetSearch.java
@@ -1,11 +1,12 @@
 package TweetSearch;
 
+import java.util.LinkedList;
 import java.util.*;
 
 public class MainTweetSearch {
 
 	public static void main(String[] args) {
-		
+
 		// Create the Hashtag Tree
 		BinarySearchTree<String> hashTagTree = new BinarySearchTree<String>();
 
@@ -15,7 +16,7 @@ public class MainTweetSearch {
 		String delimiter = " ";
 		String line = fileInput.readLine();
 		String[] initialTag = line.split(delimiter);
-		
+
 		// set value for the first hashtag
 		String hashtag = initialTag[1];
 
@@ -30,14 +31,29 @@ public class MainTweetSearch {
 		}// end of while loop
 
 		// Ask for a tweet to search for
-		System.out.println("Please enter a hashtag to search for.");
-		@SuppressWarnings("resource")
+		System.out
+				.println("Please enter a tweet and I will see if your hashtags are in my table.");
 		Scanner tweetInput = new Scanner(System.in);
-		String key = tweetInput.next();
 
-		// Search the tree and print out result of found or not
-		System.out.println(hashTagTree.findHashtag(hashTagTree, key)+" was found.");
-		
+		// Create a new list to store string words in
+		LinkedList<String> tweetHashTags = new LinkedList<String>();
+		String tweet = tweetInput.nextLine();
+		tweetInput.close();
+		// Split words from tweet into an array and then search the array for
+		// hashtags to add to the hashtag list
+		String[] tweetWords = tweet.split(delimiter);
+		for (int i = 0; i < tweetWords.length; i++) {
+			if (tweetWords[i].contains("#"))
+				tweetHashTags.addFirst(tweetWords[i]);
+		}// end of for loop
+
+		// While the hashtag list still has hashtags, search the tree and print
+		// out result of found or not
+		while (!tweetHashTags.isEmpty()) {
+			System.out.println(hashTagTree.findHashtag(hashTagTree,
+					tweetHashTags.getLast()) + " was found.");
+			tweetHashTags.removeLast();
+		}// end of while loop
 
 	}// end of main method
 
